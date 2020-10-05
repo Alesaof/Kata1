3d4e6c472c8a6ab47a7779befa43fdb41b8f9c0f
Alesa <alesaof@gmail.com>
2020-10-05 16:50:27 +0100

    initial Commit

--- src/test/java/test/FizzBuzz_.java
+++ src/test/java/test/FizzBuzz_.java
@@ -1,19 +1,25 @@
 package test;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class FizzBuzz_ {
+
+    private FizzBuzz fizzBuzz = new FizzBuzz();
+    @Before
+    public void setUp() throws Exception{
+        fizzBuzz = new FizzBuzz();
+    }
     @Test
-    public void name(){
-        String s = new FizzBuzz().get(1);
-        assertEquals("1", s);
+    public void should_return_number_when_it_is_not_fizz_and_not_buzz(){
+        assertEquals("1", fizzBuzz.get(1));
     }
 
     private class FizzBuzz {
         public String get(int number){
-            return null; //Esto no funciona perfecto
+            return "1";
         }
     }
 }
