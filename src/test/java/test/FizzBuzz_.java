bb8aaa77e71efa469d332b2e7ff4633b550e0e1d
Alesa <alesaof@gmail.com>
2020-10-05 16:51:48 +0100

    0.0.1

--- src/test/java/test/FizzBuzz_.java
+++ src/test/java/test/FizzBuzz_.java
@@ -6,12 +6,13 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class FizzBuzz_ {
-
     private FizzBuzz fizzBuzz = new FizzBuzz();
+
     @Before
     public void setUp() throws Exception{
         fizzBuzz = new FizzBuzz();
     }
+
     @Test
     public void should_return_number_when_it_is_not_fizz_and_not_buzz(){
         assertEquals("1", fizzBuzz.get(1));
