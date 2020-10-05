23a9a1225da0d590223b7b2cb1e70209aa9ddb1c
Alesa <alesaof@gmail.com>
2020-10-05 17:13:14 +0100

    0.0.2

--- src/test/java/test/FizzBuzz_.java
+++ src/test/java/test/FizzBuzz_.java
@@ -3,7 +3,8 @@ package test;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class FizzBuzz_ {
     private FizzBuzz fizzBuzz = new FizzBuzz();
@@ -15,12 +16,18 @@ public class FizzBuzz_ {
 
     @Test
     public void should_return_number_when_it_is_not_fizz_and_not_buzz(){
-        assertEquals("1", fizzBuzz.get(1));
+        assertThat(fizzBuzz.of(1)).isEqualTo("1");
+        assertThat(fizzBuzz.of(2)).isEqualTo("2");
     }
 
-    private class FizzBuzz {
-        public String get(int number){
-            return "1";
+    @Test
+    public void should_return_fizz_when_it_is_3(){
+        assertThat(fizzBuzz.of(3)).isEqualTo("Fizz");
+    }
+
+    public class FizzBuzz {
+        public String of(int number){
+            return number == 3 ? "Fizz" : String.valueOf(number);
         }
     }
 }
